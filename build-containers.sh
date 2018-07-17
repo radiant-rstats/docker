f0c9eee16193225a88dbcd547f61b34dbc528f46
vnijs <radiant@rady.ucsd.edu>
2018-07-17 14:49:26 -0700

    cache

--- build-containers.sh
+++ build-containers.sh
@@ -2,8 +2,8 @@ git pull
 docker login
 docker build -t $USER/r-bionic ./r-bionic
 docker push $USER/r-bionic
-# docker build -t $USER/radiant ./radiant
-docker build --no-cache -t $USER/radiant ./radiant
+docker build -t $USER/radiant ./radiant
+#docker build --no-cache -t $USER/radiant ./radiant
 docker push $USER/radiant
 docker build -t $USER/rsm-msba ./rsm-msba
 docker push $USER/rsm-msba
