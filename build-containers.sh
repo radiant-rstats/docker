b950276dce6bc350afa116767a1bfa9ef3b571e4
vnijs <radiant@rady.ucsd.edu>
2018-10-02 14:38:50 -0700

    pandoc 2.3.1
    Install the latest version of pandoc to address https://github.com/rocker-org/rocker-versioned/issues/103

--- build-containers.sh
+++ build-containers.sh
@@ -1,17 +1,18 @@
 git pull
 docker login
-VERSION=0.8.0
+VERSION=0.8.2
 
 ## r-bionic
 docker build -t $USER/r-bionic:latest ./r-bionic
+## docker build --no-cache -t $USER/r-bionic:latest ./r-bionic
 docker tag $USER/r-bionic:latest $USER/r-bionic:${VERSION}
 docker push $USER/r-bionic:${VERSION}; docker push $USER/r-bionic:latest
 
 ## radiant
 ## docker build -t $USER/radiant:latest ./radiant
-docker build --no-cache -t $USER/radiant ./radiant
-docker tag $USER/radiant:latest $USER/radiant:${VERSION}
-docker push $USER/radiant:${VERSION}; docker push $USER/radiant:latest
+## docker build --no-cache -t $USER/radiant ./radiant
+## docker tag $USER/radiant:latest $USER/radiant:${VERSION}
+## docker push $USER/radiant:${VERSION}; docker push $USER/radiant:latest
 
 ## rsm-msba
 docker build -t $USER/rsm-msba:latest ./rsm-msba
