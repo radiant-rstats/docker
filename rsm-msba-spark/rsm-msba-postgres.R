4f8dc5f4dc6528cd0fbec98bdb4d393bb7e0deae
vnijs <radiant@rady.ucsd.edu>
2018-11-21 23:28:46 +0000

    dplyr output

--- rsm-msba-spark/rsm-msba-postgres.R
+++ rsm-msba-spark/rsm-msba-postgres.R
@@ -9,3 +9,13 @@ con <- dbConnect(
   dbname = "postgres",
   password = "postgres"
 )
+
+library(dplyr)
+flights_db <- tbl(con, "flights")
+
+flights_db %>% select(year:day, dep_delay, arr_delay)
+flights_db %>% filter(dep_delay > 240)
+flights_db %>%
+  group_by(dest) %>%
+  summarise(delay = mean(dep_time))
+
