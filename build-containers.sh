5b54a24f17f01780c83108dc1b6d93bdb9cfe844
vnijs <radiant@rady.ucsd.edu>
2018-12-20 20:54:15 -0500

    build function

--- build-containers.sh
+++ build-containers.sh
@@ -1,45 +1,67 @@
 git pull
 docker login
-VERSION=1.0.0
+DOCKERHUB_VERSION=1.0.1
+UPLOAD="NO"
+# UPLOAD="YES"
 
-## r-bionic
-IMAGE=r-bionic
-docker build -t $USER/${IMAGE}:latest ./${IMAGE}
-# docker build --no-cache -t $USER/${IMAGE} ./${IMAGE}
-docker tag $USER/${IMAGE}:latest $USER/${IMAGE}:${VERSION}
-docker push $USER/${IMAGE}:${VERSION}; docker push $USER/${IMAGE}:latest
+build () {
+  {
+    if [[ "$1" == "NO" ]]; then
+      docker build --build-arg DOCKERHUB_VERSION=${DOCKERHUB_VERSION} --no-cache -t $USER/${LABEL}:latest ./${LABEL}
+    else
+      docker build --build-arg DOCKERHUB_VERSION=${DOCKERHUB_VERSION} -t $USER/${LABEL}:latest ./${LABEL}
+    fi
+  } || {
+    echo "-----------------------------------------------------------------------"
+    echo "Docker build for ${LABEL} was not successful"
+    echo "-----------------------------------------------------------------------"
+    sleep 3s
+    exit 1
+  }
+  if [ "${UPLOAD}" == "YES" ]; then
+    docker tag $USER/${LABEL}:latest $USER/${LABEL}:${DOCKERHUB_VERSION}
+    docker push $USER/${LABEL}:${DOCKERHUB_VERSION}; docker push $USER/${LABEL}:latest
+  fi
+}
 
-## radiant
-IMAGE=radiant
-docker build -t $USER/${IMAGE}:latest ./${IMAGE}
-# docker build --no-cache -t $USER/${IMAGE} ./${IMAGE}
-docker tag $USER/${IMAGE}:latest $USER/${IMAGE}:${VERSION}
-docker push $USER/${IMAGE}:${VERSION}; docker push $USER/${IMAGE}:latest
+# what os is being used
+ostype=`uname`
+if [[ "$ostype" == "Darwin" ]]; then
+  sed_fun () {
+    sed -i '' -e $1 $2
+  }
+else
+  sed_fun () {
+    sed -i $1 $2
+  }
+fi
 
-## rsm-msba
-IMAGE=rsm-msba
-docker build -t $USER/${IMAGE}:latest ./${IMAGE}
-# docker build --no-cache -t $USER/${IMAGE} ./${IMAGE}
-docker tag $USER/${IMAGE}:latest $USER/${IMAGE}:${VERSION}
-docker push $USER/${IMAGE}:${VERSION}; docker push $USER/${IMAGE}:latest
+launcher () {
+  cp -p ./launch-rsm-msba.sh ./launch-${LABEL}.sh
+  sed_fun "s/^LABEL=\"rsm-msba\"/LABEL=\"${LABEL}\"/" ./launch-${LABEL}.sh
+  sed_fun "s/launch-rsm-msba\.sh/launch-${LABEL}\.sh/" ./launch-${LABEL}.sh
+}
 
-## rsm-msba-spark
-IMAGE=rsm-msba-spark
-docker build -t $USER/${IMAGE}:latest ./${IMAGE}
-# docker build --no-cache -t $USER/${IMAGE} ./${IMAGE}
-docker tag $USER/${IMAGE}:latest $USER/${IMAGE}:${VERSION}
-docker push $USER/${IMAGE}:${VERSION}; docker push $USER/${IMAGE}:latest
+LABEL=r-bionic
+build
 
-## rsm-jupyterhub
-IMAGE=rsm-jupyterhub
-docker build -t $USER/${IMAGE}:latest ./${IMAGE}
-## docker build --no-cache -t $USER/${IMAGE} ./${IMAGE}
-docker tag $USER/${IMAGE}:latest $USER/${IMAGE}:${VERSION}
-docker push $USER/${IMAGE}:${VERSION}; docker push $USER/${IMAGE}:latest
+LABEL=radiant
+build
 
-## rsm-msba-beakerx
-#IMAGE=rsm-msba-beakerx
-#docker build -t $USER/${IMAGE}:latest ./${IMAGE}
-## docker build --no-cache -t $USER/${IMAGE} ./${IMAGE}
-#docker tag $USER/${IMAGE}:latest $USER/${IMAGE}:${VERSION}
-#docker push $USER/${IMAGE}:${VERSION}; docker push $USER/${IMAGE}:latest
+LABEL=rsm-msba
+build
+
+LABEL=rsm-msba-spark
+build
+launcher
+
+LABEL=rsm-jupyterhub
+build
+
+# LABEL=rsm-msba-beakerx
+# build
+# launcher
+
+# git add .
+# git commit -m "Update to image version ${DOCKHUB_VERSION}"
+# git push
