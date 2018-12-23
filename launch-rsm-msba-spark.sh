ca5f43add901ee2180414314b2dc1402b93a56c6
vnijs <radiant@rady.ucsd.edu>
2018-12-23 10:52:49 -0800

    update

--- launch-rsm-msba-spark.sh
+++ launch-rsm-msba-spark.sh
@@ -41,8 +41,9 @@ ostype=`uname`
 if [ "$ostype" == "Linux" ] || [ "$ostype" == "Darwin" ]; then
   ## check if script is already running and script is not udating
   nr_running=$(ps | grep "${LABEL}.sh" -c)
-  if [ "$nr_running" -gt 3 ] && [ "$UPDATE_SCRIPT" == "" ]; then
+  if [ "$nr_running" -gt 3 ] && [ "$UPDATE_SCRIPT" != "TRUE" ]; then
     # clear
+    echo "UPDATE? $UPDATE_SCRIPT"
     echo "-----------------------------------------------------------------------"
     echo "The ${LABEL}.sh launch script is already running (or open)"
     echo "To close the new session and continue with the old session"
