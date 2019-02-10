11e2d9bd967b7c725cadf36eec1a603d69e96aae
vnijs <radiant@rady.ucsd.edu>
2019-02-09 21:46:36 -0800

    volume create

--- launch-postgres-win.sh
+++ launch-postgres-win.sh
@@ -6,7 +6,14 @@ script_home () {
   echo "$(echo "$( cd "$(dirname "$0")" ; pwd -P )" | sed -E "s|^/([A-z]{1})/|\1:/|")"
 }
 
+## create docker volume
+has_volume=$(docker volume ls | awk "/pg_data/" | awk '{print $2}')
+if [ "${has_volume}" == "" ]; then
+  docker volume create --name=pg_data
+fi
+# docker network create "rsm-msba-spark"
+
 ## change to some other path to use as default
 SCRIPT_HOME="$(script_home)"
-
 docker-compose -f ${SCRIPT_HOME}/postgres/docker-postgres-win.yml up -d
+
