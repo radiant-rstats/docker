9540643993b9c4e0c3a76e37738ec3989c88be5e
vnijs <radiant@rady.ucsd.edu>
2019-09-05 10:32:58 -0700

    chrome-book

--- install/install-docker-chromebook.sh
+++ install/install-docker-chromebook.sh
@@ -8,8 +8,11 @@ sudo apt-get install \
   software-properties-common \
   wget
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
 
 sudo apt-get update
 sudo apt-get install docker-ce
