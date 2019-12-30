ad759147fabef52b3ce9c98a507b50c6d2559a92
vnijs <radiant@rady.ucsd.edu>
2019-12-29 16:59:15 -0800

    userconf

--- r-bionic/userconf.sh
+++ r-bionic/userconf.sh
@@ -7,5 +7,10 @@ RPASSWORD=${RPASSWORD:=rstudio}
 # works for jupyterlab but conflicts with rstudio on alternate port
 # export JPASSWORD=${JPASSWORD:=jupyter}
 
+if [[ ${DISABLE_AUTH} == "true" ]]; then
+	mv /etc/rstudio/disable_auth_rserver.conf /etc/rstudio/rserver.conf
+	echo "USER=$NB_USER" >> /etc/environment
+fi
+
 # add a password for user
 echo "$NB_USER:$RPASSWORD" | chpasswd
