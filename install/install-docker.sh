689d221c21a44d0ecaeb9bd3b3851ad855fe0b4e
vnijs <vnijs@ucsd.edu>
2020-05-24 22:40:14 -0700

    Ubuntu 20.04

--- install/install-docker.sh
+++ install/install-docker.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common git
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
 apt-cache policy docker-ce
 sudo apt install docker-ce
