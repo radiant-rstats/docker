f87633207d724f559ca07d59b281f5c6531a4be3
vnijs <vnijs@ucsd.edu>
2020-07-27 13:36:25 -0700

    cleanup

--- scripts/pull-containers.sh
+++ scripts/pull-containers.sh
@@ -1,5 +1,5 @@
 ## pull the latest version of all generated from radiant-stats/docker 
-img_list=(r-bionic radiant rsm-msba rsm-msba-spark rsm-jupyterhub)
+img_list=(r-focal radiant rsm-msba rsm-msba-spark rsm-jupyterhub rsm-vscode)
 
 for img in ${img_list[@]}; do
    docker pull vnijs/${img}
