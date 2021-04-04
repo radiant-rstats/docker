d3f8e3854547f557ab4c6cb89d824625a8efb55e
vnijs <radiant@rady.ucsd.edu>
2021-04-04 14:50:19 -0700

    1.9.0

--- scripts/build-containers.sh
+++ scripts/build-containers.sh
@@ -2,10 +2,9 @@
 
 # git pull
 docker login
-DOCKERHUB_VERSION=1.8.7
+DOCKERHUB_VERSION=1.9.0
 UPLOAD="NO"
 UPLOAD="YES"
-
 
 build () {
   {
@@ -48,16 +47,9 @@ launcher () {
   fi
 }
 
-LABEL=rsm-jupyterhub
-build
-
-## new containers should be launched using the newest version of the container
-docker tag vnijs/rsm-jupyterhub:latest jupyterhub-test-user
-
-exit
 
 LABEL=r-focal
-build
+build 
 
 # if you use the line below, manually remove the 'allow' section afterwards
 # launcher "radiant" "Radiant" "shiny-apps"
@@ -91,7 +83,7 @@ LABEL=rsm-jupyterhub
 build
 
 ## new containers should be launched using the newest version of the container
-# docker tag vnijs/rsm-jupyterhub:latest jupyterhub-user
+docker tag vnijs/rsm-jupyterhub:latest jupyterhub-user
 
 ## new containers should be launched using the newest version of the container
 docker tag vnijs/rsm-jupyterhub:latest jupyterhub-test-user
