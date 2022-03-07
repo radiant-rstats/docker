aa307236e275b8b7b7835b047ad3dcb59190d0dd
vnijs <vnijs@ucsd.edu>
2022-03-06 20:18:19 -0800

    ml

--- files/setup-ml-frameworks.sh
+++ files/setup-ml-frameworks.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 set -e
 
-mamba install -y -c \
-  tensorflow \
-  keras 
-
 if [ "$(uname -m)" != "aarch64" ]; then
-  # ML frameworks that are not (yet) available for ARM64
-  # 'pytorch' \
-  # 'pyro-ppl' \
-  # 'jax' \
-  # 'jaxlib' \
-  mamba install -y -c \
+  mamba install --quiet --yes \
+    tensorflow \
+    keras \
     numpyro
 fi
 
-mamba clean --all -f -y \
-  && fix-permissions "${CONDA_DIR}"
