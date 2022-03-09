438a25bfc94a5d1fc02ee9930d848dd67f617ffd
vnijs <vnijs@ucsd.edu>
2022-03-08 19:30:19 -0800

    2.2.0

---
+++ files/setup-bml.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+UBUNTU_VERSION=${UBUNTU_VERSION:-`lsb_release -sc`}
+CRAN=${CRAN:-https://cran.r-project.org}
+
+##  mechanism to force source installs if we're using RSPM
+CRAN_SOURCE=${CRAN/"__linux__/$UBUNTU_VERSION/"/""}
+
+## source install if using RSPM and arm64 image
+if [ "$(uname -m)" = "aarch64" ]; then
+  CRAN=$CRAN_SOURCE
+fi
+
+NCPUS=${NCPUS:--1}
+
+/usr/local/bin/R -e "install.packages(c('cmdstanr', 'posterior', 'bayesplot'), repo='${CRAN}', Ncpus=${NCPUS})" \
+
+rm -rf /tmp/downloaded_packages
