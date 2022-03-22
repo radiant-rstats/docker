0de22c5833d73ea197fbedac33fd7c306578c0a8
Vincent Nijs <vnijs@users.noreply.github.com>
2022-03-22 16:40:39 -0700

    Delete postgres-install.sh

--- postgres/postgres-install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# script to use for SQL+ETL course
-cd ~
-mkdir sql_data
-
-usethis --dest './sql_data/' "https://www.dropbox.com/s/a02acwp9amg5ukc/WestCoastImporters_Full_Dump.sql?dl=1"
-
-createdb -p 8765  -U jovyan WestCoastImporters
-psql -p 8765 WestCoastImporters -U jovyan < './sql_data/WestCoastImporters_Full_Dump.sql'
