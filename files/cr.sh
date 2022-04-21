a9243a688b59ea6a3b1931513351876daad4641b
vnijs <radiant@rady.ucsd.edu>
2022-04-20 18:12:32 -0700

    cr

--- files/cr.sh
+++ files/cr.sh
@@ -11,18 +11,22 @@ if [ "$1" != "" ]; then
     read cont
     if [ "${cont}" != "y" ]; then
         echo "The conda_remove function requires the name of a conda envivronment to remove"
-        echo "Note that if a jupyter kernel with the same exists, it will be removed as well"
+        echo "Note that if a jupyter kernel with the same name exists, it will be removed as well"
     else
         conda_remove $1
+
+        echo ""
+        echo "The code run in this function is:"
+        echo ""
+        # declare -f conda_remove
+        echo "conda remove -y -n $1 --all"
+        echo "jupyter kernelspec remove -y $1"
+
+        echo "\nSee the link below for additional information about conda"
+        echo "https://docs.conda.io/projects/conda/en/latest/user-guide/index.html"
     fi
 else
     echo "The conda_remove function requires the name of a conda envivronment to remove"
-    echo "Note that if a jupyter kernel with the same exists, it will be removed as well"
+    echo "Note that if a jupyter kernel with the same name exists, it will be removed as well"
 fi
 
-echo ""
-echo "The code run in this function is:"
-declare -f conda_remove
-
-echo "\nSee the link below for additional information about conda"
-echo "https://docs.conda.io/projects/conda/en/latest/user-guide/index.html"
