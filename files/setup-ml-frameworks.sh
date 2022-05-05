cc1ab01c6a810e5d00dbcc014f6405b66d7de681
vnijs <vnijs@ucsd.edu>
2022-05-04 23:40:32 -0700

    pytorch fix

--- files/setup-ml-frameworks.sh
+++ files/setup-ml-frameworks.sh
@@ -12,11 +12,15 @@ else
   git clone --recursive https://github.com/pytorch/pytorch
   cd pytorch
   git checkout 201ddafc22e22c387b4cd654f397e05354d73d09
+  git submodule sync
+  git submodule update --init --recursive --jobs 0
   python setup.py install
 
   git clone https://github.com/pytorch/vision.git
   cd vision
   git checkout ecbff88a1ad605bf04d6c44862e93dde2fdbfc84
+  git submodule sync
+  git submodule update --init --recursive --jobs 0
   python setup.py install
 
   cd ..
