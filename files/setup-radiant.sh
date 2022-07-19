1bb9b3226216f0273ad417267eccc33e68c350ca
vnijs <vnijs@ucsd.edu>
2022-07-19 01:30:57 -0700

    radiant from gh

--- files/setup-radiant.sh
+++ files/setup-radiant.sh
@@ -27,20 +27,21 @@ apt-get update -qq && apt-get -y --no-install-recommends install \
     && rm -rf /var/lib/apt/lists/*
 
 R -e "install.packages('igraph', repo='${CRAN}', Ncpus=${NCPUS})" \
-  -e "install.packages(c('radiant', 'gitgadget', 'miniUI', 'webshot', 'tinytex', 'svglite'), repo='${CRAN}', Ncpus=${NCPUS})" \
+  -e "install.packages(c('radiant', 'png', 'bslib', 'gitgadget', 'miniUI', 'webshot', 'tinytex', 'svglite'), repo='${CRAN}', Ncpus=${NCPUS})" \
   -e "install.packages(c('devtools', 'remotes', 'formatR', 'styler', 'reticulate', 'renv'), repo='${CRAN}', Ncpus=${NCPUS})" \
   -e "install.packages(c('arrow', 'duckdb', 'fs', 'janitor', 'palmerpenguins', 'stringr', 'tictoc'), repo='${CRAN}', Ncpus=${NCPUS})" \
   -e "install.packages(c('httpgd', 'languageserver'), repo='${CRAN}', Ncpus=${NCPUS})" \
   -e 'remotes::install_github("radiant-rstats/radiant.update", upgrade = "never")' \
+  -e "remotes::install_github('vnijs/gitgadget')" \
+  -e 'remotes::install_github("vnijs/DiagrammeR", upgrade = "never")' \
+  -e "remotes::install_github('IRkernel/IRkernel')"  \
+  -e "remotes::install_github('IRkernel/IRdisplay')" \
+  -e "IRkernel::installspec(user=FALSE)" \
   -e 'remotes::install_github("radiant-rstats/radiant.data", upgrade = "never")' \
   -e 'remotes::install_github("radiant-rstats/radiant.design", upgrade = "never")' \
   -e 'remotes::install_github("radiant-rstats/radiant.basics", upgrade = "never")' \
   -e 'remotes::install_github("radiant-rstats/radiant.model", upgrade = "never")' \
-  -e 'remotes::install_github("radiant-rstats/radiant.multivariate", upgrade = "never")' \
-  -e 'remotes::install_github("vnijs/DiagrammeR", upgrade = "never")' \
-  -e "remotes::install_github('vnijs/gitgadget')" \
-  -e "remotes::install_github('IRkernel/IRkernel')"  \
-  -e "remotes::install_github('IRkernel/IRdisplay')" \
-  -e "IRkernel::installspec(user=FALSE)"
+  -e 'remotes::install_github("radiant-rstats/radiant.multivariate", upgrade = "never")'
+
 
 rm -rf /tmp/downloaded_packages
