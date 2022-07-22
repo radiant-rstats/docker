dd8242a0821af0dc22e85b830d162dee45c08222
vnijs <vnijs@ucsd.edu>
2022-07-21 23:11:03 -0700

    dbRemoveTables

--- postgres/postgres-connect.R
+++ postgres/postgres-connect.R
@@ -1,7 +1,6 @@
 library(DBI)
-library(RPostgres)
 con <- dbConnect(
-  dbDriver("Postgres"),
+  RPostgres::Postgres(),
   user = "jovyan",
   host = "127.0.0.1",
   port = 8765,
@@ -13,21 +12,24 @@ con <- dbConnect(
 db_tabs <- dbListTables(con)
 db_tabs
 
-## add a table to the dbase
+## add a dataframe that is available in R by default to the dbase
 library(dplyr)
 if (!"mtcars" %in% db_tabs) {
   copy_to(con, mtcars, "mtcars", temporary = FALSE)
 }
 
-## extra data from dbase connection
+## extract data from dbase connection
 dat <- tbl(con, "mtcars")
 dat
 
 ## show updated list of tables
 dbListTables(con)
 
-## drop a table (not working anymore)
-# dbRemoveTable(con, table = "mtcars")
+## drop a table
+# dbRemoveTable(con, "mtcars")
 
 ## show updated list of tables
 # dbListTables(con)
+
+## disconnect from database
+dbDisconnect(con)
