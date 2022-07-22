64c4a237c3b7f80e880fc56d9c5ee4d21b5e1323
vnijs <vnijs@ucsd.edu>
2022-07-21 23:30:49 -0700

    install_github radiant

--- files/setup-radiant.sh
+++ files/setup-radiant.sh
@@ -41,6 +41,7 @@ R -e "install.packages('igraph', repo='${CRAN}', Ncpus=${NCPUS})" \
   -e "remotes::install_github('radiant-rstats/radiant.design', upgrade = 'never')" \
   -e "remotes::install_github('radiant-rstats/radiant.basics', upgrade = 'never')" \
   -e "remotes::install_github('radiant-rstats/radiant.model', upgrade = 'never')" \
-  -e "remotes::install_github('radiant-rstats/radiant.multivariate', upgrade = 'never')"
+  -e "remotes::install_github('radiant-rstats/radiant.multivariate', upgrade = 'never')" \
+  -e "remotes::install_github('radiant-rstats/radiant', upgrade = 'never')"
 
 rm -rf /tmp/downloaded_packages
