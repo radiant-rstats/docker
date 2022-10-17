b26d865137c9832eebabbdee5e1e8d2b5659966b
vnijs <vnijs@ucsd.edu>
2022-10-16 18:56:19 -0700

    conda r-base

--- files/setup-ml-frameworks.sh
+++ files/setup-ml-frameworks.sh
@@ -13,8 +13,8 @@ else
   git clone --recursive https://github.com/pytorch/pytorch
   cd pytorch
   # git checkout 201ddafc22e22c387b4cd654f397e05354d73d09
-  git checkout 8a5cc940e39820ad61dedee5c01f366af182ff3a
-  # git checkout 945d333ae485673d7a603ca71822c9a39ca4775a
+  # git checkout 8a5cc940e39820ad61dedee5c01f366af182ff3a
+  git checkout 945d333ae485673d7a603ca71822c9a39ca4775a
   git submodule sync
   git submodule update --init --recursive --jobs 0
   python setup.py install
@@ -22,8 +22,8 @@ else
   git clone https://github.com/pytorch/vision.git
   cd vision
   # git checkout ecbff88a1ad605bf04d6c44862e93dde2fdbfc84
-  git checkout fb7f9a16628cb0813ac958da4525247e325cc3d2
-  # git checkout f467349ce0d41c23695538add22f6fec5a30ece4
+  # git checkout fb7f9a16628cb0813ac958da4525247e325cc3d2
+  git checkout f467349ce0d41c23695538add22f6fec5a30ece4
   git submodule sync
   git submodule update --init --recursive --jobs 0
   python setup.py install
