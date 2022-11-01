905f4ea33bc433902be658a39fc287b1f4f06431
vnijs <vnijs@ucsd.edu>
2022-11-01 08:47:00 -0700

    highcharter

--- files/setup-extra.sh
+++ files/setup-extra.sh
@@ -17,11 +17,20 @@ fi
 NCPUS=${NCPUS:--1}
 
 if [ -f "/opt/conda/bin/R" ]; then
-  mamba install --quiet --yes -c conda-forge \
-    r-rgdal \
-    r-raster \
-    imagemagick \
-    libgdal
+  if [ "$(uname -m)" == "aarch64" ]; then
+    mamba install --quiet --yes -c conda-forge \
+      r-raster \
+      imagemagick \
+      libgdal \
+
+      R -e "install.packages('rgdal', repo='${CRAN}', Ncpus=${NCPUS})"
+  else
+    mamba install --quiet --yes -c conda-forge \
+      r-raster \
+      imagemagick \
+      libgdal \
+      r-rgdal
+  fi
 else
   export DEBIAN_FRONTEND=noninteractive
   apt-get update -qq && apt-get -y install \
@@ -36,10 +45,13 @@ else
     libgeos-dev \
     libproj-dev \
     && rm -rf /var/lib/apt/lists/*
+
+  R -e "install.packages('rgdal', repo='${CRAN}', Ncpus=${NCPUS})"
 fi
 
 R -e "install.packages(c('magick', 'leaflet', 'devtools'), repo='${CRAN}', Ncpus=${NCPUS})" \
   -e "install.packages(c('simmer', 'simmer.plot', 'EnvStats', 'matrixStats'), repo='${CRAN}', Ncpus=${NCPUS})" \
+  -e "install.packages(c('highcharter', 'viridis'), repo='${CRAN}', Ncpus=${NCPUS})" \
   -e "remotes::install_github('vnijs/webshot', upgrade = 'never')" \
   -e "webshot::install_phantomjs()"
 
