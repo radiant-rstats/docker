10e5c0000bcf340e07bc1d1e6a34fd5749fda635
rsm-vnijs <vnijs@ucsd.edu>
2023-07-29 20:19:38 -0700

    cleanup

--- scripts/pull-containers.sh
+++ scripts/pull-containers.sh
@@ -1,5 +1,4 @@
 ## pull the latest version of all generated from radiant-stats/docker 
-# img_list=(r-focal radiant rsm-msba rsm-msba-spark rsm-msba-intel-jupyterhub rsm-vscode)
 img_list=(rsm-msba-arm rsm-msba-intel rsm-msba-intel-jupyterhub)
 
 for img in ${img_list[@]}; do
