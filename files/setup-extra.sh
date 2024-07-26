0bc54c10a36b114885a494363577702d26f44199
vnijs <vnijs@ucsd.edu>
2024-07-25 19:36:31 -0700

    slimdown to webshot + phantomjs

--- files/setup-extra.sh
+++ files/setup-extra.sh
@@ -16,43 +16,43 @@ fi
 
 NCPUS=${NCPUS:--1}
 
-if [ -f "/opt/conda/bin/R" ]; then
-  if [ "$(uname -m)" == "aarch64" ]; then
-    mamba install --quiet --yes -c conda-forge \
-      r-raster \
-      imagemagick \
-      libgdal
+# if [ -f "/opt/conda/bin/R" ]; then
+#   if [ "$(uname -m)" == "aarch64" ]; then
+#     mamba install --quiet --yes -c conda-forge \
+#       r-raster \
+#       imagemagick \
+#       libgdal
 
-      # R -e "install.packages('rgdal', repo='${CRAN}', Ncpus=${NCPUS})"
-  else
-    mamba install --quiet --yes -c conda-forge \
-      r-raster \
-      imagemagick \
-      libgdal \
-      r-rgdal
-  fi
-else
-  export DEBIAN_FRONTEND=noninteractive
-  apt-get update -qq && apt-get -y install \
-    libcurl4-openssl-dev \
-    libssl-dev \
-    imagemagick \
-    libmagick++-dev \
-    gsfonts \
-    libpng-dev \
-    libgdal-dev \
-    gdal-bin \
-    libgeos-dev \
-    libproj-dev \
-    libicu-dev \
-    && rm -rf /var/lib/apt/lists/*
+#       # R -e "install.packages('rgdal', repo='${CRAN}', Ncpus=${NCPUS})"
+#   else
+#     mamba install --quiet --yes -c conda-forge \
+#       r-raster \
+#       imagemagick \
+#       libgdal \
+#       r-rgdal
+#   fi
+# else
+#   export DEBIAN_FRONTEND=noninteractive
+#   apt-get update -qq && apt-get -y install \
+#     libcurl4-openssl-dev \
+#     libssl-dev \
+#     imagemagick \
+#     libmagick++-dev \
+#     gsfonts \
+#     libpng-dev \
+#     libgdal-dev \
+#     gdal-bin \
+#     libgeos-dev \
+#     libproj-dev \
+#     libicu-dev \
+#     && rm -rf /var/lib/apt/lists/*
 
-  # R -e "install.packages('rgdal', repo='${CRAN}', Ncpus=${NCPUS})"
-fi
+#   # R -e "install.packages('rgdal', repo='${CRAN}', Ncpus=${NCPUS})"
+# fi
 
 # R -e "install.packages(c('magick', 'leaflet', 'stringi', 'profvis'), repo='${CRAN}', Ncpus=${NCPUS})"
-R -e "install.packages('devtools', repo='${CRAN}', Ncpus=${NCPUS})" \
-  -e "remotes::install_github('vnijs/webshot', upgrade = 'never')" \
+# R -e "install.packages('devtools', repo='${CRAN}', Ncpus=${NCPUS})" \
+R -e "remotes::install_github('vnijs/webshot', upgrade = 'never')" \
   -e "webshot::install_phantomjs()"
   # -e "install.packages(c('tidytext', 'wordcloud'), repo='${CRAN}', Ncpus=${NCPUS})"
 
