d77f5b7ab786a99ba51a7c679a14cc0d15039ab1
vnijs <vnijs@ucsd.edu>
2024-10-04 15:36:09 -0700

    extensions on server

--- vscode/extension-install.sh
+++ vscode/extension-install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ ! -f extensions.txt ]; then
+  wget https://raw.githubusercontent.com/radiant-rstats/docker/master/vscode/extensions.txt
+fi
+
 cat extensions.txt | while read extension || [[ -n $extension ]];
 do
   code --install-extension $extension --force
